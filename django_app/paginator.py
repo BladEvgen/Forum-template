70fcd3a5157427ee6b64bc6ecaa9f9ad9aa5f858
Evgeniy <87201663+BladEvgen@users.noreply.github.com>
2025-03-13 17:41:29 +0300

    paginator

---
+++ django_app/paginator.py
@@ -0,0 +1,17 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def paginate_queryset(request, queryset, per_page=10):
+    try:
+        per_page = int(per_page)
+    except (ValueError, TypeError):
+        per_page = 10
+    paginator = Paginator(queryset, per_page)
+    page = request.GET.get("page")
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+    return paginated_queryset
